c2e63af2b770ebb1a6afc8253767714df5163c8f
Chathuratharushka <chathuratharushka@gmail.com>
2022-07-26 18:35:58 +0530

    signup page

--- grama-app/src/App.js
+++ grama-app/src/App.js
@@ -3,19 +3,23 @@ import Main from './pages/Main'
 import Footer from './components/Footer'
 import './App.css';
 import Apply from './pages/Apply';
-
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Signup from "./pages/Signup"
 function App() {
   return (
     <div>
-      {/* <Main/> */}
-
-      <Apply/>
-      
+      <Router>
+        <Routes>
+          <Route path="/apply" element={<Apply />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+        </Routes>
+      </Router>
       <div className='footer'>
         <Footer/>
       </div>
+      
     </div>
-  
+    
   )
 }
 
